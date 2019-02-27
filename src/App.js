65b277806f25ed12e597d3debfd9cc77eb74eff7
David Rose <doppler@gmail.com>
2019-02-27 16:36:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = () => {
+  const graphDivs = 3;
+  const graphSize = 250;
+
+  return (
+    <div id="App">
+      <div id="Scene">
+        <div id="Cube">
+          <Face {...{ graphSize, graphDivs, id: "front" }} />
+          <Face {...{ graphSize, graphDivs, id: "left" }} />
+          <Face {...{ graphSize, graphDivs, id: "right" }} />
+          <Face {...{ graphSize, graphDivs, id: "back" }} />
+          <Face {...{ graphSize, graphDivs, id: "top" }} />
+          <Face {...{ graphSize, graphDivs, id: "bottom" }} />
+        </div>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default App;
+
+const Face = ({ id, graphSize, graphDivs }) => (
+  <div className="Face" id={id} style={{ width: graphSize, height: graphSize }}>
+    <svg width={graphSize} height={graphSize}>
+      {[...Array(graphDivs + 1).keys()].map(i => {
+        return (
+          <g key={i} id="grid">
+            <line
+              x1={0}
+              x2={graphSize}
+              y1={0 + (graphSize / graphDivs) * i}
+              y2={0 + (graphSize / graphDivs) * i}
+            />
+            <line
+              y1={0}
+              y2={graphSize}
+              x1={0 + (graphSize / graphDivs) * i}
+              x2={0 + (graphSize / graphDivs) * i}
+            />
+          </g>
+        );
+      })}
+    </svg>
+  </div>
+);
