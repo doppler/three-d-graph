72b7ff1d9e3bcf8cf43517aed160b7a4c14f5a08
David Rose <doppler@gmail.com>
2019-02-27 17:12:09 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  const graphDivs = 3;
+  const graphDivs = 5;
   const graphSize = 250;
 
   return (
     <div id="App">
       <div id="Scene">
         <div id="Cube">
-          <Face {...{ graphSize, graphDivs, id: "front" }} />
-          <Face {...{ graphSize, graphDivs, id: "left" }} />
-          <Face {...{ graphSize, graphDivs, id: "right" }} />
-          <Face {...{ graphSize, graphDivs, id: "back" }} />
-          <Face {...{ graphSize, graphDivs, id: "top" }} />
-          <Face {...{ graphSize, graphDivs, id: "bottom" }} />
+          {["front", "left", "right", "back", "top", "bottom"].map(side => (
+            <Face {...{ graphSize, graphDivs, id: side }} />
+          ))}
         </div>
       </div>
     </div>
@@ -26,20 +23,11 @@ const Face = ({ id, graphSize, graphDivs }) => (
   <div className="Face" id={id} style={{ width: graphSize, height: graphSize }}>
     <svg width={graphSize} height={graphSize}>
       {[...Array(graphDivs + 1).keys()].map(i => {
+        let w = 0 + (graphSize / graphDivs) * i;
         return (
           <g key={i} id="grid">
-            <line
-              x1={0}
-              x2={graphSize}
-              y1={0 + (graphSize / graphDivs) * i}
-              y2={0 + (graphSize / graphDivs) * i}
-            />
-            <line
-              y1={0}
-              y2={graphSize}
-              x1={0 + (graphSize / graphDivs) * i}
-              x2={0 + (graphSize / graphDivs) * i}
-            />
+            <line x1={0} x2={graphSize} y1={w} y2={w} />
+            <line y1={0} y2={graphSize} x1={w} x2={w} />
           </g>
         );
       })}
