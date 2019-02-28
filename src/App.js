93e69efb5b1075d327985f0d89fa043e2db27e6c
David Rose <doppler@gmail.com>
2019-02-27 21:34:51 -0500

    rainbow sine wave on spinning cube

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  const graphDivs = 5;
+  const graphDivs = 1;
   const graphSize = 250;
 
   return (
     <div id="App">
       <div id="Scene">
         <div id="Cube">
-          {["front", "left", "right", "back", "top", "bottom"].map(side => (
+          {["front", "left", "right", "back"].map(side => (
             <Face {...{ graphSize, graphDivs, id: side }} />
           ))}
         </div>
@@ -28,9 +28,25 @@ const Face = ({ id, graphSize, graphDivs }) => (
           <g key={i} id="grid">
             <line x1={0} x2={graphSize} y1={w} y2={w} />
             <line y1={0} y2={graphSize} x1={w} x2={w} />
+            <SineWave {...{ graphSize, graphDivs }} />
           </g>
         );
       })}
     </svg>
   </div>
 );
+
+const SineWave = ({ graphSize, graphDivs }) => {
+  const divs = 200;
+  return [...Array(divs).keys()].map(i => (
+    <circle
+      key={i}
+      cx={(graphSize / divs) * i}
+      cy={
+        graphSize / 2 + (Math.sin(((Math.PI * 2) / divs) * i) * graphSize) / 2
+      }
+      r={1}
+      style={{ fill: `hsla(${(360 / divs) * i}, 100%, 50%)` }}
+    />
+  ));
+};
